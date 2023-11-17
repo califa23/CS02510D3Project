a9eac184323f06143a8aa55cc3da8d56f7d27dd9
John Califano <califa23@students.rowan.edu>
2023-11-17 00:02:17 -0500

    added clock and time sync

--- main.js
+++ main.js
@@ -1,111 +1,145 @@
 import allPathCoordinatesMap from './coordinates.js';
-const mapSvg = d3.select('#map-container');
+
+const svg = d3.select('#map-container');
 const clockSvg = d3.select('#clock-container');
+const added = new Set();
+
+const speedMultiplier = 200;
+let globalClock = new Date("July 1, 2015 01:58:10"); // Initialize global clock as a Date object
+
+const carMovements = [];
+const carTypeColorMap = {
+    '1': 'cyan',
+    '2': 'blue',
+    '3': 'yellow',
+    '4': 'pink',
+    '5': 'orange',
+    '6': 'purple',
+    '2P': 'green'
+};
+// Function to check if it's time to start moving a car
+function isTimeToMoveCar(timestamp) {
+    return globalClock.getTime() >= timestamp;
+}
+
+// Function to update the global clock
+function updateGlobalClock() {
+    globalClock.setSeconds(globalClock.getSeconds() + 1);
+    updateClockText();
+    if(carMovements.length != 0){   
+        const movement = carMovements[0];
+        if(isTimeToMoveCar(movement.startTime)){
+            if(!added.has(movement.carId)){
+                svg.append('circle')
+                .attr('cx', movement.coords[0][0])
+                .attr('cy', movement.coords[0][1])
+                .attr('r', 10)
+                .style('fill', carTypeColorMap[movement.carType])
+                .attr('id', `car-${movement.carId}`);
+                added.add(movement.carId);
+            }
+            moveAlongCoordinates(`#car-${movement.carId}`, movement.coords, movement.endTime - movement.startTime);
+            carMovements.shift();
+        }
+    }
+}
+
+// Function to update the clock text with date and time
+function updateClockText() {
+    const formattedTime = d3.timeFormat('%Y-%m-%d %H:%M:%S')(globalClock);
+    clockSvg.select('text').remove();
+    clockSvg.append('text')
+        .attr('x', 20)
+        .attr('y', 50)
+        .text(formattedTime)
+        .attr('fill', 'black')
+        .style('font-size', '40px');
+}
+
+// Periodically update the global clock every second
+setInterval(updateGlobalClock, 1000/speedMultiplier);
+
 
 function delay(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-function updateClockText(timestamp) {
-    // Remove the previous text
-    clockSvg.select('text').remove();
+async function buildCarMovements(sortedData) {
 
-    // Append a new text element
-    clockSvg.append('text')
-        .attr('x', 20)
-        .attr('y', 100)
-        .text( d3.timeFormat('%Y-%m-%d %H:%M:%S')(new Date(timestamp)))
-        .attr('fill', 'black')
-        .style('font-size', '100px');
-}
+    // Create a dictionary to store gates for each car-id
+    const gatesByCarId = {};
 
-d3.csv('data/7-1-2015.csv').then(async data => {
-    data = data.sort((a, b) => new Date(a['Timestamp']).getTime() - new Date(b['Timestamp']).getTime());
-    //const filteredData = data;
-    const filteredData = data.filter(entry => entry['car-type'].includes('2P'));
-    //const filteredData = data.filter(entry => entry['car-id'].includes('20150201040237-719'));
-    //const filteredData = data.filter(entry => entry['car-id'].includes('20155801035840-804'));
+    // Populate the dictionary
+    sortedData.forEach(val => {
+        const carId = val['car-id'];
+        const gate = val['gate-name'].replace(/-/g, '~');
+        const time = new Date(val['Timestamp']).getTime();
 
-    const uniqueCarIds = [...new Set(filteredData.map(d => d['car-id']))];
-    const carTypeColorMap = {
-        '1': 'cyan',
-        '2': 'blue',
-        '3': 'yellow',
-        '4': 'pink',
-        '5': 'orange',
-        '6': 'purple',
-        '2P': 'green'
-    };
-
-    // Create a circle for each unique car-id and append it to the SVG
-    uniqueCarIds.forEach(carId => {
-        const carData = filteredData.find(d => d['car-id'] === carId);
-
-        mapSvg.append('circle')
-            .attr('cx', -100) 
-            .attr('cy', -100) 
-            .attr('r', 10) 
-            .style('fill', carTypeColorMap[carData['car-type']])
-            .attr('id', `car-${carId}`); // Set a unique id for each circle
+        if (!gatesByCarId[carId]) {
+            gatesByCarId[carId] = [];
+        }
+        gatesByCarId[carId].push([gate, time]);
     });
 
-    const nestedData = d3.nest()
-        .key(d => d['car-id'])
-        .entries(filteredData);
+    for (let i = 0; i < sortedData.length - 1; i++) {
+        const val = sortedData[i];
+        const carId = val['car-id'];
+        const gateAndTime = gatesByCarId[carId].shift();
+        const nextGateAndTime = gatesByCarId[carId][0];
+        const carType = val['car-type'];
 
-        console.log(nestedData);
+        if(nextGateAndTime != undefined){
+            const gate = gateAndTime[0];
+            const time = gateAndTime[1];
+            const nextGate = nextGateAndTime[0];
+            const endTime = nextGateAndTime[1];
 
-    const carMovements = nestedData.map(async carData => {
-        let endTime;
-        let nextGate;
-        
+            if(gate != nextGate){
+                const coords = allPathCoordinatesMap[gate + "-" + nextGate];
 
-        for (let i = 0; i < carData.values.length; i++) {
-            const val = carData.values[i];
-            const timestamp = new Date(val['Timestamp']).getTime();
-            const gate = val['gate-name'].replace(/-/g, '~');
-            nextGate = i < carData.values.length - 1 ? carData.values[i + 1]['gate-name'].replace(/-/g, '~'): '';
-            endTime = i < carData.values.length - 1 ? new Date(carData.values[i + 1]['Timestamp']).getTime() : timestamp;
-
-            updateClockText(timestamp);
-
-            if (nextGate !== '' && gate != nextGate) {
-                //console.log(gate +'-'+ nextGate);
-                const coords = allPathCoordinatesMap[gate + "-" + nextGate]
-                if(coords == undefined)
-                    console.log('MISSING: ' + gate +'-'+ nextGate);
-                if(nextGate.includes('entrance')){
-                    coords.push([-100,-100]);
+                if (coords == undefined) {
+                    console.log('MISSING: ' + gate + '-' + nextGate);
                 }
-                await moveAlongCoordinates(`#car-${carData.key}`, coords, endTime-timestamp);
+
+                carMovements.push({
+                    carId: carId,
+                    coords: coords,
+                    startTime: time,
+                    endTime: endTime,
+                    carType : carType,
+                });
             }
         }
-    });
-
-    // Wait for all cars to finish their movements before continuing
-    await Promise.all(carMovements);
-});
+    }
+}
 
 // Function to move the object to new coordinates
 function moveObject(selector, newX, newY) {
-    if(newX == -100 && newY == -100){ // hide circle when it hits an entrance (leaving)
-        d3.select(selector).style("opacity", 0);
+    if (newX == -100 && newY == -100) {
+        d3.select(selector).style('opacity', 0);
     }
     d3.select(selector)
         .transition()
-        .duration(300) // Duration of the transition in milliseconds
-        .attr("cx", newX)
-        .attr("cy", newY);
-    
-
+        .duration(10) // Duration of the transition in milliseconds
+        .attr('cx', newX)
+        .attr('cy', newY);
 }
 
+// Function to move along coordinates with delay
 async function moveAlongCoordinates(selector, coordinates, timeToTravel) {
-    const distance = coordinates.length
-    const t = timeToTravel/60000;
-    const del = t/distance * 1000;
+    const distance = coordinates.length;
+    const t = timeToTravel;
+    const del = t / distance;
     for (const coordinate of coordinates) {
         moveObject(selector, coordinate[0], coordinate[1]);
-        await delay(del); // Wait before the next move
+        await delay(del/speedMultiplier); // Wait before the next move
     }
 }
+
+// read data from csv
+d3.csv('data/7-1-2015.csv').then(async data => {
+    const filteredData = data;
+
+    const sortedData = filteredData.sort((a, b) => new Date(a['Timestamp']).getTime() - new Date(b['Timestamp']).getTime());
+    await buildCarMovements(sortedData);
+});
